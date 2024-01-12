9025dd61773087ec3d5e51a6493abb6a66cf2e3d
kent.buianh <kent.buianh@atware.asia>
2024-01-12 19:15:38 +0700

    add mantine ui and re-do side bar

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import { EuiProvider } from '@elastic/eui';
+import {EuiProvider} from '@elastic/eui';
 import '@elastic/eui/dist/eui_theme_light.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './index.css';
 import RootLayout from './pages/Root';
 import CameraDashboard from './pages/cameras/list/CameraDashboard.jsx';
@@ -11,41 +11,49 @@ import ErrorPage from './pages/errors/ErrorPage';
 import EventDashboard from './pages/events/EventDashboard';
 import StreamDashboard from './pages/streams/list/StreamDashboard.jsx';
 import StreamViewer from './pages/streams/viewer/StreamViewer';
+import '@mantine/core/styles.css';
+import {createTheme, MantineProvider} from '@mantine/core';
+
+const theme = createTheme({
+    //NONE FOR NOW
+});
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
+    {
         path: '/',
-        element: <Dashboard />,
-      },
-      {
-        path: '/streams',
-        element: <StreamDashboard />,
-      },
-      {
-        path: '/streams/cameras',
-        element: <CameraDashboard />,
-      },
-      {
-        path: '/streams/views/:streamId',
-        element: <StreamViewer />,
-      },
-      {
-        path: '/detection/events',
-        element: <EventDashboard />,
-      },
-    ],
-  },
+        element: <RootLayout/>,
+        errorElement: <ErrorPage/>,
+        children: [
+            {
+                path: '/',
+                element: <Dashboard/>,
+            },
+            {
+                path: '/streams',
+                element: <StreamDashboard/>,
+            },
+            {
+                path: '/streams/cameras',
+                element: <CameraDashboard/>,
+            },
+            {
+                path: '/streams/views/:streamId',
+                element: <StreamViewer/>,
+            },
+            {
+                path: '/detection/events',
+                element: <EventDashboard/>,
+            },
+        ],
+    },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <EuiProvider colorMode='light'>
-      <RouterProvider router={router} />
-    </EuiProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <MantineProvider>
+            <EuiProvider colorMode='light'>
+                <RouterProvider router={router}/>
+            </EuiProvider>
+        </MantineProvider>
+    </React.StrictMode>
 );
